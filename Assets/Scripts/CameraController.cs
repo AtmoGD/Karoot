06bf8e540e86887f8f6c78f61b4fc6c98d15abff
Dennis Hawran <dennishawran@gmail.com>
2023-02-03 17:21:21 +0100

    Character Movement & Camera & Bounding Box

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothSpeed = 0.125f;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float toleranceY = 2f;
+
+    private float minY;
+
+    void Update()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        float smoothedPositionY = Mathf.Lerp(transform.position.y, desiredPosition.y, smoothSpeed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, smoothedPositionY, transform.position.z);
+
+        if (transform.position.y < minY - toleranceY)
+            transform.position = new Vector3(transform.position.x, minY - toleranceY, transform.position.z);
+
+        if (transform.position.y > minY)
+            minY = transform.position.y;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
@@ -12,16 +13,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float groundCheckRadius = 0.1f;
     [SerializeField] private LayerMask groundLayer;
     public bool IsGrounded { get { return Physics.OverlapSphere(groundCheck.position, groundCheckRadius, groundLayer).Length > 0; } }
+    public bool WasGrounded { get { return Time.time - lastGroundedTime < coyoteTime; } }
 
     [SerializeField] private float speed = 5f;
     [SerializeField] private float lerpSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float coyoteTime = 0.2f;
     [SerializeField] private float gravity = 9.81f;
     [SerializeField] private float gravityLerpSpeed = 5f;
 
 
     private Vector2 moveInput;
     private bool jumpInput;
+    private float lastGroundedTime;
 
     private void Awake()
     {
@@ -30,7 +34,10 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (jumpInput && IsGrounded)
+        if (IsGrounded)
+            lastGroundedTime = Time.time;
+
+        if (jumpInput && WasGrounded)
             Jump();
 
         Move();
@@ -63,6 +70,11 @@ public class PlayerController : MonoBehaviour
         newVelocity.y = Mathf.Lerp(rb.velocity.y, -gravity, gravityLerpSpeed * Time.deltaTime);
 
         rb.velocity = newVelocity;
+    }
+
+    public void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
 #if UNITY_EDITOR
