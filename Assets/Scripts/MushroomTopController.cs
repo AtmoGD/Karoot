597fb70a217f71059f9b9b84410fddbfc99e98f0
Dennis Hawran <dennishawran@gmail.com>
2023-02-04 20:51:50 +0100

    Started Mosquito

--- Assets/Scripts/MushroomTopController.cs
+++ Assets/Scripts/MushroomTopController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MushroomTopController : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
     private void OnTriggerEnter(Collider other)
     {
         PlayerController player = other.GetComponent<PlayerController>();
